e3f98c3a710de1f79a16fd62710c358052b0ab7b
Philipp Michel <michel@apiomat.com>
2019-08-08 11:31:32 +0200

    Removed readonly flag for finalName to be able to set this from outside

--- src/main/java/com/apiomat/helper/mvnnmhelper/mojos/UploadNMMojo.java
+++ src/main/java/com/apiomat/helper/mvnnmhelper/mojos/UploadNMMojo.java
@@ -10,23 +10,17 @@
  * thum */
 package com.apiomat.helper.mvnnmhelper.mojos;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.net.HttpURLConnection;
-import java.net.URI;
-import java.net.URL;
-
 import org.apache.http.client.utils.URIBuilder;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.shared.utils.StringUtils;
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URL;
 
 /**
  * Goal to upload a native module artifact
@@ -63,17 +57,17 @@ public class UploadNMMojo extends AbstractRequestMojo
 	/**
 	 * Name of the generated JAR.
 	 */
-	@Parameter( defaultValue = "${project.build.finalName}", readonly = true )
+	@Parameter( defaultValue = "${project.build.finalName}")
 	private String finalName;
 
 	@Override
 	public void executeRequest( ) throws MojoExecutionException, IOException
 	{
 
-		final URI hostUrl = buildHostUrl( );
-		final URL url = hostUrl.toURL( );
+		URI hostUrl = buildHostUrl( );
+		URL url = hostUrl.toURL( );
 
-		final HttpURLConnection connection = ( HttpURLConnection ) url.openConnection( );
+		HttpURLConnection connection = ( HttpURLConnection ) url.openConnection( );
 		connection.setDoOutput( true );
 		connection.setRequestProperty( "Content-Type", "application/octet-stream" );
 		connection.setRequestMethod( "POST" );
@@ -89,14 +83,14 @@ public class UploadNMMojo extends AbstractRequestMojo
 
 		connection.setRequestProperty( "Authorization", getUserAuthHeaderValue( ) );
 
-		final File jarFile = getJarFile( this.moduleJarPath, this.finalName, "NM" );
+		File jarFile = getJarFile( this.moduleJarPath, this.finalName, "NM" );
 		if ( jarFile.exists( ) == false )
 		{
 			throw new MojoExecutionException( "Can't find module jar in " + jarFile.getAbsolutePath( ) );
 		}
-		final OutputStream writer = connection.getOutputStream( );
-		final FileInputStream fis = new FileInputStream( jarFile );
-		final byte[ ] buf = new byte[ 1024 ];
+		OutputStream writer = connection.getOutputStream( );
+		FileInputStream fis = new FileInputStream( jarFile );
+		byte[ ] buf = new byte[ 1024 ];
 		System.out.print( "Writing bytes " );
 		for ( int c = fis.read( buf ); c != -1; c = fis.read( buf ) )
 		{
@@ -107,18 +101,18 @@ public class UploadNMMojo extends AbstractRequestMojo
 		writer.close( );
 		fis.close( );
 		System.out.println( "" );
-		final int responseCode = connection.getResponseCode( );
+		int responseCode = connection.getResponseCode( );
 		if ( responseCode != HttpURLConnection.HTTP_CREATED )
 		{
-			try (final InputStream is =
+			try (InputStream is =
 				( 200 <= responseCode && responseCode <= 299 ) ? connection.getInputStream( )
 					: connection.getErrorStream( ))
 			{
 				String reason = "";
 				if ( is != null )
 				{
-					final StringBuilder inputStringBuilder = new StringBuilder( );
-					final BufferedReader bufferedReader =
+					StringBuilder inputStringBuilder = new StringBuilder( );
+					BufferedReader bufferedReader =
 						new BufferedReader( new InputStreamReader( is, "UTF-8" ) );
 					String line = bufferedReader.readLine( );
 					while ( line != null )
@@ -145,9 +139,9 @@ public class UploadNMMojo extends AbstractRequestMojo
 	{
 		try
 		{
-			final StringBuilder sb = new StringBuilder( );
+			StringBuilder sb = new StringBuilder( );
 			sb.append( this.host ).append( "/yambas/rest/modules/asset" );
-			final URIBuilder bldr = new URIBuilder( sb.toString( ) );
+			URIBuilder bldr = new URIBuilder( sb.toString( ) );
 			if ( StringUtils.isNotBlank( this.system ) )
 			{
 				bldr.addParameter( "usedSystem", this.system );
@@ -158,7 +152,7 @@ public class UploadNMMojo extends AbstractRequestMojo
 			}
 			return bldr.build( );
 		}
-		catch ( final Exception e )
+		catch ( Exception e )
 		{
 			getLog( ).error( "Error building url", e );
 		}
@@ -173,7 +167,7 @@ public class UploadNMMojo extends AbstractRequestMojo
 	 * @param classifier an optional classifier
 	 * @return the file to generate
 	 */
-	protected static File getJarFile( final File basedir, final String resultFinalName, final String classifier )
+	protected static File getJarFile( File basedir, String resultFinalName, String classifier )
 	{
 		if ( basedir == null )
 		{
@@ -184,7 +178,7 @@ public class UploadNMMojo extends AbstractRequestMojo
 			throw new IllegalArgumentException( "finalName is not allowed to be null" );
 		}
 
-		final StringBuilder fileName = new StringBuilder( resultFinalName );
+		StringBuilder fileName = new StringBuilder( resultFinalName );
 		fileName.append( "-" ).append( classifier );
 		fileName.append( ".jar" );
 
