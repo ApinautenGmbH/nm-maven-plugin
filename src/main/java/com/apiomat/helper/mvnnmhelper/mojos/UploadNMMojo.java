cc9126b9a9093f1d6fe745ab6c3a740e87adae97
Philipp Michel <michel@apiomat.com>
2019-08-29 16:27:45 +0200

    removed generic import

--- src/main/java/com/apiomat/helper/mvnnmhelper/mojos/UploadNMMojo.java
+++ src/main/java/com/apiomat/helper/mvnnmhelper/mojos/UploadNMMojo.java
@@ -17,7 +17,13 @@ import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.shared.utils.StringUtils;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URI;
 import java.net.URL;
